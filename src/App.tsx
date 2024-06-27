a2062a9882bfdfc153cec8747ad08cd0a134d72f
shaunprice <shaundnz@gmail.com>
2024-06-27 19:34:26 +1200

    Commit app component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="mb-12 max-w-2xl space-y-4 text-center">
-        <h1 className="text-5xl font-bold ">Made For All</h1>
+        <h1 className="text-5xl font-bold ">Made For All!</h1>
         <p className="mt-4 text-xl">
-          Enter a "Made For You" Spotify playlist, and generate a playlist of
-          the the original uncustomized version.
+          Enter a &quot;Made For You&quot; Spotify playlist, and generate a
+          playlist of the the original uncustomized version.
         </p>
         <form className="flex items-center space-x-2">
           <Input type="text" placeholder="Enter Spotify playlist link" />
