c6c9a5f1aef9c7d2784c503bdf4f2b39b666bfc2
shaunprice <shaundnz@gmail.com>
2024-07-18 23:12:09 +1200

    Updated input to use custom icon

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,41 @@
+import { LucideIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: LucideIcon;
+  endIcon?: LucideIcon;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
+    const StartIcon = startIcon;
+    const EndIcon = endIcon;
+
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
+      <div className="relative w-full">
+        {StartIcon && (
+          <div className="absolute left-1.5 top-1/2 -translate-y-1/2">
+            <StartIcon size={18} className="text-muted-foreground" />
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {EndIcon && (
+          <div className="absolute right-3 top-1/2 -translate-y-1/2">
+            <EndIcon className="text-muted-foreground" size={18} />
+          </div>
+        )}
+      </div>
     );
   },
 );
